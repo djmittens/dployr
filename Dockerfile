9b4c47d10d26f289988fb4a2eb7da871f5445993
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-01-17 01:25:51 -0800

    fixing up some problems with stuff

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 
-RUN apk add --no-cache ansible ssh
+RUN apk add --no-cache ansible openssh
 
 ADD ./machine.py /machine.py
 ADD ./playbooks /playbooks
@@ -8,4 +8,4 @@ ADD ./conf/ansible.cfg /etc/ansible/ansible.cfg
 ADD ./entrypoint.sh /entrypoint.sh
 
 entrypoint ["./entrypoint.sh"]
-cmd ["/playbooks/bootstrap/.yml"]
+cmd ["/playbooks/bootstrap.yml"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if[[! -d /root/.ssh]]; then
+if [[ ! -d /root/.ssh ]]; then
     echo "Missing host's ssh configuration, please mount it into /root/.ssh"
     exit 1
 fi
 
-ansible all -i "localhost," -m raw -a "apt-get install -y python-minimal"
+ansible all -i "${TARGET_HOST}," -m raw -a "apt-get install -y python-minimal"
 
-ansible-playbook -i "${TARGET_HOST:-localhost}," "$@"
+ansible-playbook -i "${TARGET_HOST}," "$@"
